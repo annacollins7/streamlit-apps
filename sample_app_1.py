b5a6d0f03d14f95ab99200b27226ae5b91a90611
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-07-11 12:25:09 +0200

    Update sample_app_1.py

--- sample_app_1.py
+++ sample_app_1.py
@@ -10,3 +10,4 @@ slider = st.slider('Number of Motors:', min_value=1, max_value=20)
 select_box = st.selectbox("What type of rocket?",
     ("Solid", "Hybrid", "Liquid"))
 
+button = st.button('Label')
