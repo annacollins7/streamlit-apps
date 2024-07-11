319054697528a733bcb9e78322bc680cc170304c
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-07-11 12:23:48 +0200

    Update sample_app_1.py

--- sample_app_1.py
+++ sample_app_1.py
@@ -6,3 +6,7 @@ st.title('Sample App 1')
 
 slider = st.slider('Number of Motors:', min_value=1, max_value=20)
 
+
+select_box = st.selectbox("What type of rocket?",
+    ("Solid", "Hybrid", "Liquid"))
+
