94ca0eac26415e129a06565be286adebc65990fa
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-07-18 12:19:43 +0200

    Update sample_app_1.py

--- sample_app_1.py
+++ sample_app_1.py
@@ -11,8 +11,21 @@ number_of_motors = st.slider('Number of Motors:', min_value=1, max_value=20)
 
 st.write(str(number_of_motors))
 
-select_box = st.selectbox("What type of rocket?",
+motor_type = st.selectbox("What type of rocket?",
     ("Solid", "Hybrid", "Liquid"))
+
+
+# Solid case
+if motor_type is "Solid":
+    st.write("Need to electrocute it")
+
+# Hybrid case
+
+
+# Liquid case
+
+
+
 
 
 # def calculate_square(x):
