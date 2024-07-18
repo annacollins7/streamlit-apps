5c27e9005b66896f877ad967bb67352c39be5fb6
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-07-18 11:11:18 +0200

    Update sample_app_1.py

--- sample_app_1.py
+++ sample_app_1.py
@@ -7,14 +7,16 @@ import streamlit as st
 st.title('Sample App 1')
 
 
-slider = st.slider('Number of Motors:', min_value=1, max_value=20)
+number_of_motors = st.slider('Number of Motors:', min_value=1, max_value=20)
 
+st.write(str(number_of_motors))
 
 select_box = st.selectbox("What type of rocket?",
     ("Solid", "Hybrid", "Liquid"))
 
-# def calculate_square(x):
-#     return x**2
+
+def calculate_square(x):
+    return x**2
 # if st.button('Calculate'):
 #     st.write(calculate_square(2))
 
