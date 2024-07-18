faff5173f8e1926971c97201494229f353962135
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-07-18 12:38:55 +0200

    Update sample_app_1.py

--- sample_app_1.py
+++ sample_app_1.py
@@ -9,9 +9,9 @@ st.title('Sample App 1')
 
 number_of_motors = st.slider('Number of Motors:', min_value=1, max_value=20)
 
-for i in range(number_of_motors):
-    st.selectbox("What type of motor?",
-    ("Forbidden ESP", "Solid", "Hybrid", "Liquid"))
+# for i in range(number_of_motors):
+#     st.selectbox("What type of motor?",
+#     ("Forbidden ESP", "Solid", "Hybrid", "Liquid"))
 
 st.write(str(number_of_motors))
 
