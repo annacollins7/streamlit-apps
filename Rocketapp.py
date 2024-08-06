5282b6d2dd9e15650f9e6486361c4abea2f9de4b
Anna Collins <45462492+annacollins7@users.noreply.github.com>
2024-08-06 07:29:59 +0200

    Update Rocketapp.py

--- Rocketapp.py
+++ Rocketapp.py
@@ -74,7 +74,7 @@ elif weather_condition == "Rainy":
 # Function to simulate rocket flight and plot trajectory
 def simulate_flight():
     # Dummy motor for demonstration
-    motor = SolidMotor(thrustSource=0, burnOut=3.9, grainNumber=5, grainSeparation=5/1000, grainDensity=1815, grainOuterRadius=33/1000, grainInitialInnerRadius=15/1000, grainInitialHeight=120/1000, nozzleRadius=33/1000, throatRadius=11/1000, interpolationMethod='linear')
+    motor = SolidMotor(thrustSource=lambda x: 1 / (x + 1), burnOut=3.9, grainNumber=5, grainSeparation=5/1000, grainDensity=1815, grainOuterRadius=33/1000, grainInitialInnerRadius=15/1000, grainInitialHeight=120/1000, nozzleRadius=33/1000, throatRadius=11/1000, interpolationMethod='linear')
     
     # Rocket object
     rocket = Rocket(motor=motor, radius=127/2000, mass=19.2, inertiaI=6.60, inertiaZ=0.0351, distanceRocketNozzle=-1.255, distanceRocketPropellant=0.930, powerOffDrag=2.5, powerOnDrag=2.5)
